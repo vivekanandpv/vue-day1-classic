07c5de729fa0e354d9d6685766ed86f9b83293c1
Vivekanand P V <vivekanandpv@outlook.com>
2020-06-24 22:05:58 +0530

    done

---
+++ sample.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>{{name}}</h2>
+    <p>{{city}}</p>
+    <button @click="sayHello">Click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sample",
+  data() {
+    return {
+      name: "Vinayak",
+      city: "Hyderabad"
+    };
+  },
+  methods: {
+    sayHello() {
+      alert(`${this.name} says hello to you all!`);
+    }
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 30px;
+  color: blue;
+}
+
+p {
+  font-size: 14px;
+  color: brown;
+}
+</style>
